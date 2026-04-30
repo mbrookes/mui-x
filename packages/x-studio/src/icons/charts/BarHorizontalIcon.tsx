a4750103889f3a92c4b5d3bd60537e4704186bd2
Matt <github@nospam.33m.co>
2026-04-30 22:12:09 +0100

    Add more chart icons

---
+++ packages/x-studio/src/icons/charts/BarHorizontalIcon.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { ChartSvg, type IconProps } from '../utils';
+
+export function BarHorizontalIcon({
+  size,
+  color = 'currentColor',
+  secondaryColor = 'currentColor',
+}: IconProps) {
+  const c1 = color;
+  const c2 = secondaryColor;
+  const opacity2 = 0.45;
+  const bars = [
+    { y: 3, w: 18, c: c1 },
+    { y: 7, w: 12, c: c2, o: opacity2 },
+    { y: 13, w: 22, c: c1 },
+    { y: 17, w: 15, c: c2, o: opacity2 },
+    { y: 23, w: 14, c: c1 },
+    { y: 27, w: 9, c: c2, o: opacity2 },
+  ];
+  return (
+    <ChartSvg size={size}>
+      {bars.map((b, i) => (
+        <rect
+          key={i}
+          x={2}
+          y={b.y}
+          width={b.w}
+          height={3}
+          fill={b.c}
+          opacity={b.o ?? 1}
+          rx={0.5}
+        />
+      ))}
+    </ChartSvg>
+  );
+}
